09c4e3d2df34ccf26d8cdb7e50c71cfa5cf8f83b
Clément Lemoine <clementlemoine.cl@gmail.com>
2017-10-05 00:24:25 +0200

    Fllllyyyyyyy + kill + some future fun

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import * as THREE from 'three';
 import threeOrbitControls from './utils/OrbitControls';
 import Stats from 'stats.js';
+import {TweenMax} from 'gsap';
+import Wasp from './wasp';
 import './style.css';
+
+const NUMBER_WASP = 10;
+const wasps = [];
 
 const OrbitControls = threeOrbitControls(THREE);
 
@@ -15,16 +20,13 @@ document.body.appendChild( renderer.domElement );
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xe07a57 );
-const fov = 60;
-const aspectRatio = window.innerWidth / window.innerHeight;
+
+let aspectRatio = window.innerWidth / window.innerHeight;
 const nearPlane = 1;
 const farPlane = 10000;
 const D = 200;
-//const camera = new THREE.PerspectiveCamera( fov, aspectRatio, nearPlane, farPlane );
-
 const camera = new THREE.OrthographicCamera(-D*aspectRatio, D*aspectRatio, D, -D, nearPlane, farPlane)
 camera.position.set(-800, 800, 800);
-
 camera.lookAt(scene.position)
 
 const controls = new OrbitControls (camera, renderer.domElement);
@@ -53,183 +55,81 @@ scene.add( bulb );
 
 
 // LE SOOOOL
-
-var geoFloor = new THREE.PlaneGeometry( 2050, 2050, 32 );
-var matFloor = new THREE.MeshPhongMaterial({
+const matFloor = new THREE.MeshPhongMaterial({
     color: 0xe07a57, 
     flatShading : true, 
     side: THREE.DoubleSide
 });
-var floor = new THREE.Mesh( geoFloor, matFloor );
+
+const floor = new THREE.Mesh( new THREE.PlaneGeometry( 2050, 2050, 32 ), matFloor );
 floor.rotation.x = -Math.PI / 2;
 floor.position.y = -50;
 floor.receiveShadow = true;
 scene.add( floor );
 
-/////////  LA GUEEPE  ///////////
+// ON VERRA TOUT CA PLUS TARD
+// for(let i = 0; i <= NUMBER_WASP; i++){
+//     wasps.push(Wasp());
+// }
 
-const wasp = new THREE.Group();
+// wasps.forEach(w => {
+//     const wasp = Wasp();
+//     wasp.scale.set(0.5,0.5,0.5);
+//     wasp.position.set(Math.random() * (250 + 250) - 250, 0, Math.random() * (250 + 250) - 250);
+//     scene.add(wasp);
+// });
 
-// LE CORPS
-const bodyGroup = new THREE.Group();
-const matOrange = new THREE.MeshPhongMaterial( {
-    color: 0xf7be35, 
-    shininess:0,
-    flatShading : true, 
-    side: THREE.DoubleSide
-});
-const matBlack = new THREE.MeshPhongMaterial({
-    color: 0x202020, 
-    shininess:0,
-    flatShading : true, 
-    side: THREE.DoubleSide
-});
+const wasp = Wasp();
+wasp.wasp.scale.set(0.5,0.5,0.5);
+scene.add(wasp.wasp);
 
-const geoBodyRight = new THREE.CylinderGeometry( 9, 1, 10, 8 );
-const meshBodyRight = new THREE.Mesh( geoBodyRight, matOrange );
-meshBodyRight.rotation.z = Math.PI / 2;
-meshBodyRight.position.x = -5;
-bodyGroup.add(meshBodyRight);
-
-const geoBodyCenter = new THREE.CylinderGeometry( 9, 9, 9, 8 );
-const meshBodyCenter = new THREE.Mesh( geoBodyCenter, matBlack );
-meshBodyCenter.rotation.z = Math.PI / 2;
-meshBodyCenter.position.x = -14.5;
-bodyGroup.add(meshBodyCenter);
-
-const geoBodyLeft = new THREE.CylinderGeometry( 1, 9, 10, 8 );
-const meshBodyLeft = new THREE.Mesh( geoBodyLeft, matOrange );
-meshBodyLeft.rotation.z = Math.PI / 2;
-meshBodyLeft.position.x = -24;
-bodyGroup.add(meshBodyLeft);
-
-
-bodyGroup.rotation.z = 0.1;
-
-wasp.add(bodyGroup);
-
-
-// LE DARD
-const dardGroup = new THREE.Group();
-
-const geoDardStart = new THREE.CylinderGeometry( 1, 9, 10, 8 );
-const meshDardStart = new THREE.Mesh( geoDardStart, matBlack );
-meshDardStart.rotation.z = Math.PI / 2;
-meshDardStart.position.x = 5;
-dardGroup.add(meshDardStart);
-
-const geoDardCenter = new THREE.CylinderGeometry( 9, 12, 12, 8 );
-const meshDardCenter = new THREE.Mesh( geoDardCenter, matOrange );
-meshDardCenter.rotation.z = Math.PI / 2;
-meshDardCenter.position.x = 16;
-dardGroup.add(meshDardCenter);
-
-const geoDardCenter2 = new THREE.CylinderGeometry( 12, 11, 10, 8 );
-const meshDardCenter2 = new THREE.Mesh( geoDardCenter2, matBlack );
-meshDardCenter2.rotation.z = Math.PI / 2;
-meshDardCenter2.position.x = 27;
-dardGroup.add(meshDardCenter2);
-
-const geoDardCenter3 = new THREE.CylinderGeometry( 11, 9, 10, 8 );
-const meshDardCenter3 = new THREE.Mesh( geoDardCenter3, matOrange );
-meshDardCenter3.rotation.z = Math.PI / 2;
-meshDardCenter3.position.x = 37;
-dardGroup.add(meshDardCenter3);
-
-const geoDardEnd = new THREE.CylinderGeometry( 9, 0, 14, 8 );
-const meshDardEnd = new THREE.Mesh( geoDardEnd, matBlack );
-meshDardEnd.rotation.z = Math.PI / 2;
-meshDardEnd.position.x = 49;
-dardGroup.add(meshDardEnd);
-
-dardGroup.rotation.z = -0.2;
-dardGroup.position.x = -6;
-wasp.add(dardGroup);
-
-
-// LA TETE DE LA BETE
-const headGroup = new THREE.Group();
-
-const geoHeadStart = new THREE.ConeGeometry( 8, 10, 8, 1);
-const meshHeadStart = new THREE.Mesh( geoHeadStart, matBlack );
-geoHeadStart.vertices[0].x += 4;
-geoHeadStart.computeVertexNormals();
-geoHeadStart.computeFaceNormals();
-geoHeadStart.__dirtyVertices = true;
-geoHeadStart.__dirtyNormals = true;
-meshHeadStart.rotation.z = -Math.PI / 2;
-meshHeadStart.rotation.x = Math.PI;
-meshHeadStart.position.x = -35;
-
-headGroup.add(meshHeadStart);
-
-const geoHeadEnd = new THREE.SphereGeometry(8, 8, 4, 0, Math.PI * 2, 0, Math.PI / 2)
-const meshHeadEnd = new THREE.Mesh( geoHeadEnd, matBlack );
-meshHeadEnd.rotation.z = -Math.PI / 2;
-meshHeadEnd.rotation.y = Math.PI;
-meshHeadEnd.position.x = -40;
-
-headGroup.add(meshHeadEnd);
-
-headGroup.position.x = 8;
-headGroup.position.y = -5;
-
-wasp.add(headGroup);
-
-// LES ANTENNNNNES
-const antenne1 = new THREE.Group();
-
-const geoA1Base = new THREE.CylinderGeometry( 0.5, 0.5, 10, 32);
-const a1Base = new THREE.Mesh( geoA1Base, matBlack );
-a1Base.rotation.z = Math.PI / -2 - 0.3;
-antenne1.add(a1Base);
-const geoA1End = new THREE.CylinderGeometry( 0.5, 0, 10, 32);
-const a1End = new THREE.Mesh( geoA1End, matBlack );
-a1End.rotation.z = Math.PI / -2 + 0.3;
-a1End.position.x = -9.25;
-a1End.position.y = 0;
-antenne1.add(a1End);
-
-antenne1.position.x = -37;
-antenne1.position.y = -1;
-antenne1.position.z = -4;
-antenne1.rotation.x = -0.7;
-
-const antenne2 = new THREE.Group();
-
-const geoA2Base = new THREE.CylinderGeometry( 0.5, 0.5, 10, 32);
-const a2Base = new THREE.Mesh( geoA2Base, matBlack );
-a2Base.rotation.z = Math.PI / -2 - 0.3;
-antenne2.add(a2Base);
-const geoA2End = new THREE.CylinderGeometry( 0.5, 0, 10, 32);
-const a2End = new THREE.Mesh( geoA2End, matBlack );
-a2End.rotation.z = Math.PI / -2 + 0.3;
-a2End.position.x = -9.25;
-a2End.position.y = 0;
-antenne2.add(a2End);
-
-antenne2.position.x = -37;
-antenne2.position.y = -1;
-antenne2.position.z = 4;
-antenne2.rotation.x = 0.7;
-
-
-
-wasp.add(antenne1);
-wasp.add(antenne2);
-
-wasp.traverse(m => {
-   m.castShadow = true;
-   m.receiveShadow = true;
-});
-wasp.scale.set(0.5,0.5,0.5);
-scene.add(wasp);
 
 const animate = timestamp => {
     stats.begin();
     renderer.render(scene, camera);
+
+    // ON FAIT BOUGER LA QUEPE ET ELLE REBONDIE PAS ENCORE TRES PARFAIT
+    // wasp.translateX(-1);
+    // camera.updateMatrix();
+    // camera.updateMatrixWorld();
+
+    // const frustum = new THREE.Frustum();
+    // frustum.setFromMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse));  
+    
+    // if (!frustum.containsPoint(wasp.position)) {
+    //     const newDir = new THREE.Vector3(Math.random() * (250 + 250) - 250, 0, Math.random() * (250 + 250) - 250);
+    //     const pos = new THREE.Vector3();
+    //     pos.addVectors(newDir, wasp.position);
+    //     wasp.lookAt(pos);
+    // }
+
+
     stats.end();
     requestAnimationFrame(animate);
 }
 
+
+const resizeHandler = () => {
+    aspectRatio = window.innerWidth / window.innerHeight;
+    camera.left = -D*aspectRatio;
+    camera.right = D*aspectRatio
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const clickHandler = e => {
+    mouse.x = e.clientX / window.innerWidth * 2 - 1;
+	mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+	raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(wasp.wasp, true);
+	if (intersects.length > 0) {
+        wasp.kill();
+	}
+};
+
+
+addEventListener('click', clickHandler);
+addEventListener('resize', resizeHandler);
 animate();
--- src/wasp.js
+++ src/wasp.js
@@ -1,9 +1,200 @@
-export default (opts) => {
-    const group = new THREE.Group();
+import * as THREE from 'three';
+import TweenMax from 'gsap';
+
+export default () => {
+
+    const matOrange = new THREE.MeshPhongMaterial( {
+        color: 0xf7be35, 
+        shininess:0,
+        flatShading : true, 
+        side: THREE.DoubleSide
+    });
+    const matBlack = new THREE.MeshPhongMaterial({
+        color: 0x202020, 
+        shininess:0,
+        flatShading : true, 
+        side: THREE.DoubleSide
+    });
+    const matWhite = new THREE.MeshPhongMaterial({
+        color: 0xfafafa,
+        shininess:0,
+        flatShading : true, 
+        side: THREE.DoubleSide
+    });
+
+    const wasp = new THREE.Group();
 
 
+    // LE CORPS
+    const bodyGroup = new THREE.Group();
+
+    const bodyRight = new THREE.Mesh( new THREE.CylinderGeometry( 9, 1, 10, 8 ), matOrange );
+    bodyRight.rotation.z = Math.PI / 2;
+    bodyRight.position.x = -5;
+
+    const bodyCenter = new THREE.Mesh( new THREE.CylinderGeometry( 9, 9, 9, 8 ), matBlack );
+    bodyCenter.rotation.z = Math.PI / 2;
+    bodyCenter.position.x = -14.5;
+
+    const bodyLeft = new THREE.Mesh( new THREE.CylinderGeometry( 1, 9, 10, 8 ), matOrange );
+    bodyLeft.rotation.z = Math.PI / 2;
+    bodyLeft.position.x = -24;
+
+    bodyGroup.add(bodyRight);
+    bodyGroup.add(bodyCenter);
+    bodyGroup.add(bodyLeft);
+    bodyGroup.rotation.z = 0.1;
 
 
+    // LE DARD
+    const dardGroup = new THREE.Group();
+
+    const dardStart = new THREE.Mesh( new THREE.CylinderGeometry( 1, 9, 10, 8 ), matBlack );
+    dardStart.rotation.z = Math.PI / 2;
+    dardStart.position.x = 5;
     
-    return group;
-}
+    const dardCenter = new THREE.Mesh( new THREE.CylinderGeometry( 9, 12, 12, 8 ), matOrange );
+    dardCenter.rotation.z = Math.PI / 2;
+    dardCenter.position.x = 16;
+    
+    const dardCenter2 = new THREE.Mesh( new THREE.CylinderGeometry( 12, 11, 10, 8 ), matBlack );
+    dardCenter2.rotation.z = Math.PI / 2;
+    dardCenter2.position.x = 27;
+    
+    const dardCenter3 = new THREE.Mesh( new THREE.CylinderGeometry( 11, 9, 10, 8 ), matOrange );
+    dardCenter3.rotation.z = Math.PI / 2;
+    dardCenter3.position.x = 37;
+    
+    const dardEnd = new THREE.Mesh( new THREE.CylinderGeometry( 9, 0, 14, 8 ), matBlack );
+    dardEnd.rotation.z = Math.PI / 2;
+    dardEnd.position.x = 49;
+
+    dardGroup.add(dardStart);
+    dardGroup.add(dardCenter);
+    dardGroup.add(dardCenter2);
+    dardGroup.add(dardCenter3);
+    dardGroup.add(dardEnd);
+    dardGroup.rotation.z = -0.2;
+    dardGroup.position.x = -6;
+
+
+    // LA TETE DE LA BETE
+    const headGroup = new THREE.Group();
+
+    const geoHeadStart = new THREE.ConeGeometry( 8, 10, 8, 1);
+    const headStart = new THREE.Mesh( geoHeadStart, matBlack );
+    geoHeadStart.vertices[0].x += 4;
+    geoHeadStart.computeVertexNormals();
+    geoHeadStart.computeFaceNormals();
+    geoHeadStart.__dirtyVertices = true;
+    geoHeadStart.__dirtyNormals = true;
+    headStart.rotation.z = -Math.PI / 2;
+    headStart.rotation.x = Math.PI;
+    headStart.position.x = -35;
+
+    const headEnd = new THREE.Mesh(new THREE.SphereGeometry(8, 8, 4, 0, Math.PI * 2, 0, Math.PI / 2), matBlack );
+    headEnd.rotation.z = -Math.PI / 2;
+    headEnd.rotation.y = Math.PI;
+    headEnd.position.x = -40;
+
+    headGroup.add(headStart);
+    headGroup.add(headEnd);
+    headGroup.position.x = 8;
+    headGroup.position.y = -5;
+    
+
+    // LES ANTENNNNNES
+    const antenne1 = new THREE.Group();
+    const antenne2 = new THREE.Group();
+    const geoBaseAntenne = new THREE.CylinderGeometry( 0.5, 0.5, 10, 32);
+    const geoEndAntenne = new THREE.CylinderGeometry( 0.5, 0, 10, 32);
+
+    // Antenne 1
+    const a1Base = new THREE.Mesh( geoBaseAntenne, matBlack );
+    a1Base.rotation.z = Math.PI / -2 - 0.3;
+    
+    const a1End = new THREE.Mesh( geoEndAntenne, matBlack );
+    a1End.rotation.z = Math.PI / -2 + 0.3;
+    a1End.position.x = -9.25;
+    a1End.position.y = 0;
+
+    antenne1.add(a1Base);
+    antenne1.add(a1End);
+    antenne1.position.x = -37;
+    antenne1.position.y = -1;
+    antenne1.position.z = -4;
+    antenne1.rotation.x = -0.7;
+
+    // Antenne 1
+    const a2Base = new THREE.Mesh( geoBaseAntenne, matBlack );
+    a2Base.rotation.z = Math.PI / -2 - 0.3;
+    
+    const a2End = new THREE.Mesh( geoEndAntenne, matBlack );
+    a2End.rotation.z = Math.PI / -2 + 0.3;
+    a2End.position.x = -9.25;
+    a2End.position.y = 0;
+
+    antenne2.add(a2Base);
+    antenne2.add(a2End);
+    antenne2.position.x = -37;
+    antenne2.position.y = -1;
+    antenne2.position.z = 4;
+    antenne2.rotation.x = 0.7;
+
+
+    // LES AILES
+    const geoAileG = new THREE.CylinderBufferGeometry( 30, 3, 25, 8, 1, true, -0.4, 0.8 );
+    geoAileG.translate( 0, 12.5, 0 );
+    const aileG = new THREE.Mesh(geoAileG, matWhite);
+    aileG.position.y = 4.5;
+    aileG.position.x = -14;
+    aileG.position.z = 3;
+    
+    const geoAileD = new THREE.CylinderBufferGeometry( 30, 3, 25, 8, 1, true, Math.PI - 0.4, 0.8 );
+    geoAileD.translate( 0, 12.5, 0 );
+    const aileD = new THREE.Mesh(geoAileD, matWhite);
+    aileD.position.y = 4.5;
+    aileD.position.x = -14;
+    aileD.position.z = -3;
+    
+    wasp.add(bodyGroup);
+    wasp.add(dardGroup);
+    wasp.add(headGroup);
+    wasp.add(antenne1);
+    wasp.add(antenne2);
+    wasp.add(aileG);
+    wasp.add(aileD);
+
+
+    wasp.traverse(m => {
+        m.castShadow = true;
+        m.receiveShadow = true;
+    });
+
+
+    let alive = true;
+    const kill = () => {
+        if(alive){
+            swingAileD.pause();
+            swingAileG.pause();
+        }else{
+            swingAileD.play();
+            swingAileG.play();
+        }
+        alive = !alive;
+    };
+
+    const swingAileD = TweenMax.to(aileD.rotation, 0.05, {
+        z: -3,
+        yoyo: true,
+        repeat: -1
+    });
+
+    const swingAileG = TweenMax.to(aileG.rotation, 0.05, {
+        z: -3,
+        yoyo: true,
+        repeat: -1
+    });
+
+    return {wasp, kill};
+};
